65fa937517781d0c6bb31e58bdd294dbbdeac863
heyustudio <heyustudio@gmail.com>
2014-12-02 09:42:53 +0800

    parallax

---
+++ parallax/parallax2_edgeActions.js
@@ -0,0 +1,9 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindSymbolAction(compId,symbolName,"creationComplete",function(sym,e){sym.stop();document.addEventListener('touchstart',function(event){event.preventDefault();return false;});window.ondeviceorientation=function(event){var delta=Math.round(event.beta);switch(window.orientation){case 0:delta=Math.round(event.gamma);break;case 180:delta=-Math.round(event.gamma);break;}
+var position=1000+(delta*20);position=Math.floor(position);sym.stop(position);console.log(position);}});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-976014217");
